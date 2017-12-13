7d6f53c1b0ca16ed146cdcd1bf3ddb79631df72d
dopplereffec <ilja.belka@yandex.ru>
2017-12-14 02:54:32 +0300

    Dobavil * and "

--- program1.cpp
+++ program1.cpp
@@ -10,9 +10,21 @@ double MySub(double a, double b)
 {
 return a - b;
 }
+
+ double MyMul(double a, double b)
+ { 
+	 return a * b; 
+ } 
+ 
+double MyDiv(double a, double b)
+{ 
+	return a / b;
+}
 int main()
 {
 cout << MySum(10, 3) << endl;
 cout << MySub(10, 3) << endl;
+cout << MyMul(10, 3) << endl;
+cout << MyDiv(10, 3) << endl;
 return 0;
 }
